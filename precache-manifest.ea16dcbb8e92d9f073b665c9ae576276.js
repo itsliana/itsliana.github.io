ec2b706ec14c009d650254106441f8ff81d77438
Liana Birke <liana.birke@gmail.com>
2018-10-04 02:20:59 -0400

    Updates

---
+++ precache-manifest.ea16dcbb8e92d9f073b665c9ae576276.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9767e10fb328b040d077f12287e822f",
+    "url": "/static/media/signature.d9767e10.svg"
+  },
+  {
+    "revision": "c18c712bf8a03475b1581b0fb8598866",
+    "url": "/static/media/paper.c18c712b.png"
+  },
+  {
+    "revision": "3aacb3bbed5edb5612cd7c4e3d7364e2",
+    "url": "/static/media/medusa.3aacb3bb.svg"
+  },
+  {
+    "revision": "af75994abe7f60bd1884c0b74cc5e487",
+    "url": "/static/media/favicon.af75994a.ico"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0864420b3e59513e6ca8",
+    "url": "/static/js/main.0864420b.chunk.js"
+  },
+  {
+    "revision": "72fae94408f77d4c64ee",
+    "url": "/static/js/1.72fae944.chunk.js"
+  },
+  {
+    "revision": "0864420b3e59513e6ca8",
+    "url": "/static/css/main.51d6acbb.chunk.css"
+  },
+  {
+    "revision": "10394717cd90b4ceda643676b732cf60",
+    "url": "/index.html"
+  }
+];
