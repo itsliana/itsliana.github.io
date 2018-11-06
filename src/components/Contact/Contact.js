af05c121697b638dfb8f2f4b0957e5c06a1cbd63
Liana Birke <liana.birke@gmail.com>
2018-11-06 15:20:56 -0500

    removed insta

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,9 +8,8 @@ const contact = () => (
     <p><span>Front-end Web Developer</span></p>
     <p><span className={classes.email}>liana.birke@gmail.com</span></p>
     <p><span>View my </span><a href="//www.linkedin.com/in/liana-birke-997328ba/" target="_blank" rel="noopener noreferrer">resume</a></p>
-    <p><a href="//github.com/itsliana/itsliana.github.io" target="_blank" rel="noopener noreferrer">About</a><span> this site</span></p>
-    <p><span>View my </span><a href="//github.com/itsliana/german-this" target="_blank" rel="noopener noreferrer">last project</a></p>
-    <p><span>It's about a </span><a href="//www.instagram.com/bull.child/" target="_blank" rel="noopener noreferrer">mood</a></p>
+    <p><a href="//github.com/itsliana/itsliana.github.io/tree/source" target="_blank" rel="noopener noreferrer">View</a><span> the source code</span></p>
+    <p><span>My </span><a href="//github.com/itsliana/german-this" target="_blank" rel="noopener noreferrer">last project</a></p>
   </div>
 )
 
